336b1e0652fb88c988f0784489ab43010d27aa3c
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2021-03-25 19:47:13 +0700

    initial

---
+++ SeminarWebservice2021/ClientApp/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                sgsdgsdsd
+            </div>
+        );
+    }
+}
+
+export default App;
